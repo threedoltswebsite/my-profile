de3cbfc919ea6557389f40acf480d8c119b48eca
threedoltswebsite <threedoltswebsite@gmail.com>
2024-01-06 08:04:31 -0800

    year

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,37 @@
-'use strict';
+"use strict";
 
 // navbar variables
-const nav = document.querySelector('.mobile-nav');
-const navMenuBtn = document.querySelector('.nav-menu-btn');
-const navCloseBtn = document.querySelector('.nav-close-btn');
-
+const nav = document.querySelector(".mobile-nav");
+const navMenuBtn = document.querySelector(".nav-menu-btn");
+const navCloseBtn = document.querySelector(".nav-close-btn");
 
 // navToggle function
-const navToggleFunc = function () { nav.classList.toggle('active'); }
+const navToggleFunc = function () {
+  nav.classList.toggle("active");
+};
 
-navMenuBtn.addEventListener('click', navToggleFunc);
-navCloseBtn.addEventListener('click', navToggleFunc);
-
-
+navMenuBtn.addEventListener("click", navToggleFunc);
+navCloseBtn.addEventListener("click", navToggleFunc);
 
 // theme toggle variables
-const themeBtn = document.querySelectorAll('.theme-btn');
-
+const themeBtn = document.querySelectorAll(".theme-btn");
 
 for (let i = 0; i < themeBtn.length; i++) {
-
-  themeBtn[i].addEventListener('click', function () {
-
+  themeBtn[i].addEventListener("click", function () {
     // toggle `light-theme` & `dark-theme` class from `body`
     // when clicked `theme-btn`
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
 
     for (let i = 0; i < themeBtn.length; i++) {
-
       // When the `theme-btn` is clicked,
       // it toggles classes between `light` & `dark` for all `theme-btn`.
-      themeBtn[i].classList.toggle('light');
-      themeBtn[i].classList.toggle('dark');
-
+      themeBtn[i].classList.toggle("light");
+      themeBtn[i].classList.toggle("dark");
     }
-
-  })
-
+  });
 }
+
+// Get the current year
+var currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
